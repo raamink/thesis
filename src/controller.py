2c9cd046aeea59e600f208351894c07016c3c152
rmink <minkraoul@gmail.com>
2021-03-17 12:30:21 +0100

    Add file handling for model setup

--- src/controller.py
+++ src/controller.py
@@ -6,17 +6,26 @@ Main file which controls other aspects of training / running of network.
  - arg parser for command line interfacing
 """
 
+#stdlibs
 from os.path import isdir, isfile
 from os import listdir
 from pathlib import Path
 
+#local imports
+from model import myModel
+
 class controller:
+    def __init__(self, archFolder: str = None):
+        if archFolder:
+            self.archFolder = Path(archFolder)
+            self.archFiles = self.collectFiles(archFolder)
+
     def collectFiles(self, folderName: str):
         if type(folderName) is not str:
             raise ValueError('Wrong input type to collectFiles. Expected string')
         
-        cwd = Path.cwd()
-        folder = cwd / folderName
+        # cwd = Path.cwd()
+        folder = Path(folderName)
 
         if not folder.exists():
             print(folder, folder.exists)
@@ -26,11 +35,23 @@ class controller:
         arches = []
         for entry in folder.glob('./*'):
             if entry.suffix == '.json':
-                settings.append(str(entry.relative_to(folder)))
+                # settings.append(str(entry.relative_to(folder)))
+                settings.append(entry)
             elif entry.suffix == '.csv':
-                arches.append(str(entry.relative_to(folder)))
+                # arches.append(str(entry.relative_to(folder)))
+                arches.append(entry)
             else:
                 continue
 
         return settings, arches
-    
+
+    def network(self, files: tuple) -> myModel:
+        archFile, compFile = files
+        return myModel(architectureFile=archFile, compileFile=compFile)
+
+    def nextNetwork(self, files: tuple) -> myModel:
+        parms, arches = files
+        for parm in parms:
+            for arch in arches:
+                yield self.network((arch, parm))
+
--- src/test_controller.py
+++ src/test_controller.py
@@ -4,18 +4,20 @@ from unittest import TestCase, mock
 # import os
 
 #Foreign imports
+from pathlib import Path
 # import tensorflow as tf
 # import tensorflow.keras as keras
 # import numpy as np
 
 #Local imports
 import controller
+import model
 
 class testController(TestCase):
     def setUp(self):
-        self.tester = controller.controller
+        self.tester = controller.controller()
 
-    def test_controller_fileCollection_inputs(self):
+    def test_collectFiles_inputs(self):
         self.assertRaises(ValueError, self.tester.collectFiles, 1)       # Int
         self.assertRaises(ValueError, self.tester.collectFiles, 1.0)     # Float
         self.assertRaises(ValueError, self.tester.collectFiles, [1])     # List
@@ -25,6 +27,31 @@ class testController(TestCase):
         
         self.assertRaises(ValueError, self.tester.collectFiles, 'balkjdaf') # Folder doens't exist
         
+    def test_collectFiles_outputs(self):
+        self.assertEqual(self.tester.collectFiles('tests/test1/'), 
+                         ([],[])) # Empty folder
+        self.assertEqual(self.tester.collectFiles('tests/test2/'), 
+                         ([Path('tests/test2/parms.json')],[Path('tests/test2/arch.csv')])) # Arch + Parms
+        self.assertEqual(self.tester.collectFiles('tests/test3/'), 
+                         ([],[Path('tests/test3/arch.csv'), Path('tests/test3/arch2.csv')])) # 2 Arch
+        self.assertEqual(self.tester.collectFiles('tests/test4/'), 
+                         ([Path('tests/test4/parms.json')],[Path('tests/test4/bla.csv'), Path('tests/test4/parms2.csv')])) # 2 Parms
+
+    def test_createsMyModelObject(self):
+        files = ('tests/testArch.csv','tests/testCompileParms.json')
+        output = self.tester.network(files)
+        self.assertIsInstance(output, model.myModel)
+        self.assertTrue(output.model._is_compiled)
+
+    def test_manageSequenceOfMyModels(self):
+        files = (['a.json'],
+                 ['b.csv', 'c.csv'])
+
+        with mock.patch('controller.controller.network', lambda self,x: x):
+            output = list(self.tester.nextNetwork(files))
+        
+        self.assertEqual(output, [('b.csv', 'a.json'), ('c.csv', 'a.json')])
+
     def test_controller_fileCollection_outputs(self):
         self.assertEqual(self.tester.collectFiles('src/test1/'), ([],[])) # Empty folder
         self.assertEqual(self.tester.collectFiles('src/test2/'), (['parms.json'],['arch.csv'])) # Arch + Parms
@@ -32,7 +59,7 @@ class testController(TestCase):
         self.assertEqual(self.tester.collectFiles('src/test4/'), (['parms.json'],['bla.csv', 'parms2.csv'])) # 2 Parms
         
 
-        
+
 
 if __name__ == "__main__":
     unittest.main()
